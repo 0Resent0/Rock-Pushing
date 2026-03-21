e60106b44876e91ab8531ef91d9737006c261c29
0Resent0 <81683459+0Resent0@users.noreply.github.com>
2026-03-21 21:26:08 +0700

    Locked

---
+++ Assets/Asset/Script/MapUnlockManger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MapUnlockManager : MonoBehaviour
+{
+    public Button map1Button;
+    public Button map2Button;
+
+    void Start()
+    {
+        // 🔒 ล็อกก่อน
+        map1Button.interactable = false;
+        map2Button.interactable = false;
+
+        // 🔓 ปลดล็อกตามข้อมูล
+        if (PlayerPrefs.GetInt("UnlockMap1", 0) == 1)
+            map1Button.interactable = true;
+
+        if (PlayerPrefs.GetInt("UnlockMap2", 0) == 1)
+            map2Button.interactable = true;
+    }
+}
