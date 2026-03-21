8acbae5b5fc2a83882b8f77ac8ca94fa7d166368
0Resent0 <81683459+0Resent0@users.noreply.github.com>
2026-03-22 02:43:48 +0700

    fake UPG

--- Assets/Asset/Script/Upgrade/UpgradeUi.cs
+++ Assets/Asset/Script/Upgrade/UpgradeUi.cs
@@ -6,13 +6,30 @@ public class UpgradeUI : MonoBehaviour
     public TextMeshProUGUI stableText;
     public TextMeshProUGUI recoveryText;
 
-
     void Update()
     {
-        stableText.text = "Stable: " + RockUpgradeSystem.Instance.GetStableLevel();
-        recoveryText.text = "Recovery: " + RockUpgradeSystem.Instance.GetRecoveryLevel();
+        UpdateUpgradeText();
     }
 
+    void UpdateUpgradeText()
+    {
+        var sys = RockUpgradeSystem.Instance;
+
+        UpdateSingle(stableText, sys.GetStableLevel(), sys.maxLevel);
+        UpdateSingle(recoveryText, sys.GetRecoveryLevel(), sys.maxLevel);
+    }
+
+    void UpdateSingle(TextMeshProUGUI text, int level, int max)
+    {
+        if (text == null) return;
+
+        float percent = ((float)level / max) * 100f;
+        int rounded = Mathf.RoundToInt(percent);
+
+        text.text = level + " (" + rounded + "%)";
+    }
+
+    // Button functions
     public void UpgradeStable() => RockUpgradeSystem.Instance.UpgradeStable();
     public void UpgradeRecovery() => RockUpgradeSystem.Instance.UpgradeRecovery();
 }
